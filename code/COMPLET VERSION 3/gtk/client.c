19c90d22fd0a5870583e80fc2fbb83a28c0d3e1b
guillaume.avril <guillaume.avril@epita.fr>
2021-06-15 16:32:53 +0200

    suppression COMPLET VERSION 3

--- code/COMPLET VERSION 3/gtk/client.c
+++ code/COMPLET VERSION 3/gtk/client.c
@@ -6,50 +6,45 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "interface.h"
 
 #define MAXI 200
 #define PORT 8080
 #define SA struct sockaddr
 
+#define ip_address "192.168.0.49"
+
 char* func(int sockfd)
 {
   write(sockfd, "1", 1);
   char buff[MAXI];
   ssize_t r = 0;
+
+  memset(buff, 0, MAXI);
+
+  ssize_t buff_size = 0;;
   while(r == 0)
   {
-      memset(buff,0,MAXI);
 
-      r = read(sockfd, buff, sizeof(buff));
-      if(r != 0)
-      {   
-        /*
-          write(STDOUT_FILENO, buff, r);
-          printf("\n");
-          */
-          printf("%s\n", buff);
-      }
-
+      r = read(sockfd, &buff, sizeof(buff));
       
+      buff_size += r;
       sleep(1);
   }
 
-  int n = 0;
-  
-  memset(buff,0,MAXI);
-  //printf("Enter the string : ");
-  //while ((buff[n++] = getchar()) != '\n');
-  //write(sockfd, buff, sizeof(buff));
+  char* res = calloc(buff_size, sizeof(char));
+  for (ssize_t i = 0; i < buff_size; ++i)
+  {
+  	res[i] = buff[i];
+  }
 
-  printf("From Server : %s\n", buff);
+  //printf("From Server : %s", buff);
+  //printf("res = %s\n", res);
 
-  return buff;
-  
+  return res;
 }
 
-char* client()
+Client client()
 {
   int sockfd;
   struct sockaddr_in servaddr;
@@ -66,7 +61,7 @@ char* client()
 
   // assign IP, PORT
   servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr = inet_addr("10.0.2.15");
+  servaddr.sin_addr.s_addr = inet_addr(ip_address);
   servaddr.sin_port = htons(PORT);
 
   // connect the client socket to server socket
@@ -76,13 +71,16 @@ char* client()
   }
   else
     printf("connected to the server..\n");
+
   //write(sockfd,"1",1);
   // function for chat
-  char* res = func(sockfd);
 
-  // close the socket
-  close(sockfd);
+  char* json_file = func(sockfd);
 
 
-  return res;
+  //printf("json file : %s\n", json_file);
+
+  Client cli = { sockfd, json_file };
+
+  return cli;
 }
--- code/COMPLET VERSION 3/gtk/interface.c
+++ code/COMPLET VERSION 3/gtk/interface.c
@@ -9,14 +9,13 @@ struct GetVal{
   };
 
 struct GetVal Value;
+int sockfd;
 
 
-
-int main(int argc, char* argv[])
+void create_window(int argc, char* argv[], char* json_file)
 {
 
-
-
+  //printf("json file : %s\n", json_file);
   //Init variables
   GtkWidget *main_window;
   Data data;
@@ -30,9 +29,14 @@ int main(int argc, char* argv[])
   data.builder = gtk_builder_new();
   gtk_builder_add_from_file(data.builder, "cafetiere.glade", NULL);
 
-	//Get main_window
+	//Get the objects
 	main_window =  GTK_WIDGET(gtk_builder_get_object(data.builder, "main_window"));
-  //main_window =  GTK_WIDGET(gtk_builder_get_object(data.builder, "main_window"));
+  GtkSpinButton *spinbuttonHours = GTK_SPIN_BUTTON(gtk_builder_get_object(data.builder, "spinbuttonHours"));
+  GtkSpinButton *spinbuttonMinutes = GTK_SPIN_BUTTON(gtk_builder_get_object(data.builder, "spinbuttonMinutes"));
+  GtkToggleButton *shortButton = GTK_TOGGLE_BUTTON(gtk_builder_get_object(data.builder, "Short"));
+  GtkToggleButton *longButton = GTK_TOGGLE_BUTTON(gtk_builder_get_object(data.builder, "Long"));
+
+
 
 
   //link w/ css file
@@ -41,38 +45,86 @@ int main(int argc, char* argv[])
   gtk_style_context_add_provider_for_screen(gdk_screen_get_default(), GTK_STYLE_PROVIDER(cssProvider), GTK_STYLE_PROVIDER_PRIORITY_USER);
 
 
+  char coffees[30][6];
+  for (int i = 0; i < 30; ++i)
+  {
+      memset(coffees[i], 0, 6);
+  }
+  int posCoffee = 0;
+  int c = 0;
+  int posc = 0;
 
+  /*char tmp[5];
+  memset(tmp, 0, 5);*/
 
+  while(posCoffee < 30)
+  {
+    //printf("%c\n", json_file[c]);
+    if(json_file[c] == '\n')
+    {
+      posc = 0;
 
-/*
-  //Get the list of the coffee with the API
-  //TODO
+      //printf("%s\n", coffees[posCoffee]);
+      posCoffee++;
+    }
+    else
+    {
+      coffees[posCoffee][posc] = json_file[c];
+      posc++;
+    }
+    c++;
+  }
+
   
-  //Set the value of the spinbutton1 to hours
-  int hours = 5;
+  for(int i = 0; i < 30; i++)
+  {
+    printf("%s\n", coffees[i]);
+  }
 
-  GtkSpinButton *spinbuttonHours = GTK_SPIN_BUTTON(gtk_builder_get_object(data.builder, "spinbuttonHours"));
+
+
+
+
+  char* type = calloc(5, sizeof(char));
+  type = coffees[0];
+  char* time = calloc(4, sizeof(char));
+  time = coffees[1];
+  char* activate = calloc(1, sizeof(char));
+  activate = coffees[2];
+
+  //printf("\ntime = %s\ntype = %s\nactivate = %s\n", time, type, activate);
+  //printf("coffees[0] = %s\n", coffees[0]);
+
+  char* h = calloc(2, sizeof(char));
+  strncpy(h, time, 2);
+
+  char* min = calloc(2, sizeof(char));
+  strncpy(min, time+2, 2);
+
+
+  //printf("%s:%s\n", h, min);
+
+
+  //Set the value of the spinbuttonHours to hours
+  int hours = atoi(h);
   gtk_spin_button_set_value(spinbuttonHours, hours);
 
-  char[] request = "GET / HTTP/1.1";
-
-
   //Set the value of the spinbutton2 to minutes
-  int minutes = 32;
-  
-  GtkSpinButton *spinbuttonMinutes = GTK_SPIN_BUTTON(gtk_builder_get_object(data.builder, "spinbuttonMinutes"));
+  int minutes = atoi(min);
   gtk_spin_button_set_value(spinbuttonMinutes, minutes);
 
 
-  gboolean = gtk_toggle_button_get_active();
+  int ret1 = strncmp(type, "long", 4);
+  int ret2 = strncmp(type, "court", 5);
 
-  //Set the value of the radio button the the right value
-  GtkToggleButton *shortButton = GTK_RADIO_BUTTON(gtk_builder_get_object(data.builder, "Short"));
-  gtk_toggle_button_set_active(shortButton, TRUE);
-
-  GtkToggleButton *longButton = GTK_RADIO_BUTTON(gtk_builder_get_object(data.builder, "Long"));
-  gtk_toggle_button_set_active(longButton, TRUE); 
-*/
+  if(ret1 == 0)
+  {
+    gtk_toggle_button_set_active(longButton, TRUE);
+  }
+  else if(ret2 == 0)
+  {
+    gtk_toggle_button_set_active(shortButton, TRUE);
+  }
 
 
 
@@ -81,17 +133,27 @@ int main(int argc, char* argv[])
   Value.minutes = GTK_SPIN_BUTTON(gtk_builder_get_object(data.builder, "spinbuttonMinutes"));
   Value.type = "short";
 
-
 	//Connect signals
 	gtk_builder_connect_signals(data.builder, &data);
 
   gtk_window_set_title(GTK_WINDOW(main_window), "Epitière");
   gtk_widget_show_all(main_window);
   gtk_main();
+}
 
 
-  //char* json_file = client();
-  //printf("%s\n", json_file);
+char* client_mover()
+{
+  Client cli = client();
+  sockfd = cli.sockfd;
+  return cli.json_file;
+}
+
+
+int main(int argc, char* argv[])
+{
+  char* json_file = client_mover();
+  create_window(argc, argv, json_file);
 
   return EXIT_SUCCESS;
 }
@@ -104,7 +166,6 @@ void on_validation_clicked()
 
   printf("Time set to %d:%d\n", hours, minutes);
   printf("You will get a %s coffee\n", Value.type);
-  char* json_file = client();
 }
 
 void on_Short_toggled()
@@ -118,7 +179,8 @@ void on_Long_toggled()
 }
 
 // called when window is closed
-void on_window_main_destroy()
+void on_main_window_destroy()
 {
+  close(sockfd);
   gtk_main_quit();
 }
